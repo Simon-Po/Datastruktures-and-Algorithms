9ff554631c33c55f3d6512708bd940b6c7a09a49
Simon P <97339701+Simon-Po@users.noreply.github.com>
2022-12-15 14:57:26 +0100

    Bubble Sort Implemented

---
+++ Algorithms/Bubble_sort/App.java
@@ -0,0 +1,49 @@
+package Algorithms.Bubble_sort;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+public class App {
+    public static void main(String[] args) {
+        
+        int[] arr = new int[100];
+        Random r = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = r.nextInt(0,100);
+        }
+        
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + ",");
+        // // }
+        // System.out.println("");
+        arr = bubbleSort(arr);
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + ",");
+        // }
+
+
+    }
+
+    private static int[] bubbleSort(int[] arr) {
+        int swaps = 0;
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                
+                if(arr[j] > arr[j +1]) {
+                    swaps++;
+                    int temp = arr[j];
+                    arr[j] = arr[j +1];
+                    arr[j +1] = temp;
+                    swapped = true;
+                }
+                
+            }
+            if(!swapped) {
+                break;
+            }
+        }
+        System.out.println(swaps);
+        return arr;
+    }
+}
